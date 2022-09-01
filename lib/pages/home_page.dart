45993c6a92b22d90fcfc3b6318b4b678e21c354e
eshan.srivastava <eshansrivastava.24@gmail.com>
2022-09-01 14:11:44 +0200

    day 3 completed

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -3,16 +3,16 @@ import 'package:flutter/material.dart';
 class HomePage extends StatelessWidget {
   final int days = 30;
   final String name = "flutter";
-  var naam = "tuesday";
+  var naam = "        C A T A L O G  A P P";
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text("CATALOG APP"),
+        title: Text(naam),
       ),
       body: Center(
         child: Container(
-          child: Text("welcome to $days days of studying $name $naam"),
+          child: Text("welcome to $days days of studying "),
         ),
       ),
       drawer: Drawer(),
